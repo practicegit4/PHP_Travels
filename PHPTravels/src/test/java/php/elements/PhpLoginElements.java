42965b0a0a7e0081cfbb32ac406a32e00b5423ac
robir <robir@DESKTOP-4CN1S37.home>
2019-06-24 22:41:27 -0400

    my commit 1

--- PHPTravels/src/test/java/php/elements/PhpLoginElements.java
+++ PHPTravels/src/test/java/php/elements/PhpLoginElements.java
@@ -1,5 +1,22 @@
 package php.elements;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class PhpLoginElements {
 
+	//UserName
+	@FindBy(name="username")
+	public WebElement username;
+	
+	//Password
+	@FindBy(name = "password")
+	public WebElement password;
+
+	//Login Button
+	@FindBy(xpath = "//*[@id=\"loginfrm\"]/button")
+	public WebElement loginButton;
+	
+	
+	
 }
